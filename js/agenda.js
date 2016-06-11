354bb76a520e6067de81eb1469cf043c154c80b8
Manu <gavrismanuela1993@gmail.com>
2016-06-11 11:39:52 +0300

    with JSON

--- js/agenda.js
+++ js/agenda.js
@@ -2,22 +2,26 @@ console.debug('loading agenda');
 
 
 function getRow(person){
-    var nume=person[0];
-    var telefon=person[1];
+    var nume=person.nume;
+    var prenume=person.prenume;
+    var telefon=person.telefon;
 
-    var row= '<tr><td>'+ nume +'</td><td>'+ telefon +'</td></tr>';
+    var row= '<tr><td>'+ nume +'</td><td>'+ prenume +'</td><td>'+ telefon +'</td></tr>';
     return row;
 }
 
+
+
 var contacts=[
-    ['Manu','490'],
-    ['Olimpia','0233'],
-    ['Andrei', "3709"],
-    ['Paul', "0932"]
+    {nume: 'Manu', prenume: 'D',telefon: '490'},
+    {nume: 'Olimpia', prenume: 'C',telefon: '0234'},
+    {nume:'Oana',prenume:'V',telefon:'6543'},
+    {nume:'Andrei',prenume:'B', telefon:"3709"},
+    {nume:'Paul',prenume:'A', telefon:"0932"}
 ];
 
 for(var i=0; i<contacts.length; i++){
     var person=contacts[i];
-    $('#agenda tbody').append(getRow(person));f
+    $('#agenda tbody').append(getRow(person));
 }
 
